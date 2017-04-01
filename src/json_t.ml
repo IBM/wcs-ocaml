b089fe90b2c3682ea5845d47041b37d4deb0148e
Louis Mandel <lmandel@us.ibm.com>
2017-04-01 08:22:54 -0400

    Move files.

--- src/json_t.ml
+++ src/json_t.ml
@@ -2,10 +2,5 @@ type safe = Yojson.Safe.json
 type basic = Yojson.Basic.json
 type json = basic
 
-type ('a, 'b) deriving_result = ('a, 'b) Ppx_deriving_yojson_runtime.Result.result
-
-type 'a deriving_error_or = 'a Ppx_deriving_yojson_runtime.error_or
-
-
 let write_json = Yojson.Basic.write_json
 let read_json = Yojson.Basic.read_json
