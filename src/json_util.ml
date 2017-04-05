e31973f9dc15525e8b78a1785cb5f96f6450bcf6
Louis Mandel <lmandel@us.ibm.com>
2017-04-05 17:49:10 -0400

    Add conversion functions.

--- src/json_util.ml
+++ src/json_util.ml
@@ -54,3 +54,139 @@ let json_of_list_workspaces_response rsp =
 
 let pretty_list_workspaces_response rsp =
   Yojson.Basic.pretty_to_string (json_of_list_workspaces_response rsp)
+
+
+(** {8. intent_example} *)
+
+let json_of_intent_example x =
+  Yojson.Basic.from_string (Wcs_j.string_of_intent_example x)
+
+let pretty_intent_example x =
+  Yojson.Basic.pretty_to_string (json_of_intent_example x)
+
+
+(** {8. intent_def} *)
+
+let json_of_intent_def x =
+  Yojson.Basic.from_string (Wcs_j.string_of_intent_def x)
+
+let pretty_intent_def x =
+  Yojson.Basic.pretty_to_string (json_of_intent_def x)
+
+
+(** {8. entity_value} *)
+
+let json_of_entity_value x =
+  Yojson.Basic.from_string (Wcs_j.string_of_entity_value x)
+
+let pretty_entity_value x =
+  Yojson.Basic.pretty_to_string (json_of_entity_value x)
+
+
+(** {8. entity_def} *)
+
+let json_of_entity_def x =
+  Yojson.Basic.from_string (Wcs_j.string_of_entity_def x)
+
+let pretty_entity_def x =
+  Yojson.Basic.pretty_to_string (json_of_entity_def x)
+
+
+(** {8. go_to} *)
+
+let json_of_go_to x =
+  Yojson.Basic.from_string (Wcs_j.string_of_go_to x)
+
+let pretty_go_to x =
+  Yojson.Basic.pretty_to_string (json_of_go_to x)
+
+
+(** {8. output_def} *)
+
+let json_of_output_def x =
+  Yojson.Basic.from_string (Wcs_j.string_of_output_def x)
+
+let pretty_output_def x =
+  Yojson.Basic.pretty_to_string (json_of_output_def x)
+
+
+(** {8. dialog_node} *)
+
+let json_of_dialog_node x =
+  Yojson.Basic.from_string (Wcs_j.string_of_dialog_node x)
+
+let pretty_dialog_node x =
+  Yojson.Basic.pretty_to_string (json_of_dialog_node x)
+
+
+(** {8. workspace} *)
+
+let json_of_workspace x =
+  Yojson.Basic.from_string (Wcs_j.string_of_workspace x)
+
+let pretty_workspace x =
+  Yojson.Basic.pretty_to_string (json_of_workspace x)
+
+
+(** {8. input} *)
+
+let json_of_input x =
+  Yojson.Basic.from_string (Wcs_j.string_of_input x)
+
+let pretty_input x =
+  Yojson.Basic.pretty_to_string (json_of_input x)
+
+
+(** {8. entity} *)
+
+let json_of_entity x =
+  Yojson.Basic.from_string (Wcs_j.string_of_entity x)
+
+let pretty_entity x =
+  Yojson.Basic.pretty_to_string (json_of_entity x)
+
+
+(** {8. output} *)
+
+let json_of_output x =
+  Yojson.Basic.from_string (Wcs_j.string_of_output x)
+
+let pretty_output x =
+  Yojson.Basic.pretty_to_string (json_of_output x)
+
+
+(** {8. message_request} *)
+
+let json_of_message_request x =
+  Yojson.Basic.from_string (Wcs_j.string_of_message_request x)
+
+let pretty_message_request x =
+  Yojson.Basic.pretty_to_string (json_of_message_request x)
+
+
+(** {8. message_response} *)
+
+let json_of_message_response x =
+  Yojson.Basic.from_string (Wcs_j.string_of_message_response x)
+
+let pretty_message_response x =
+  Yojson.Basic.pretty_to_string (json_of_message_response x)
+
+
+(** {8. create_response} *)
+
+let json_of_create_response x =
+  Yojson.Basic.from_string (Wcs_j.string_of_create_response x)
+
+let pretty_create_response x =
+  Yojson.Basic.pretty_to_string (json_of_create_response x)
+
+
+(* (\** {8. action} *\) *)
+
+(* let json_of_action x = *)
+(*   Yojson.Basic.from_string (Wcs_j.string_of_action x) *)
+
+(* let pretty_action x = *)
+(*   Yojson.Basic.pretty_to_string (json_of_action x) *)
+
