55b3f6241bbe510ec85cc1ad40bdebb7072f923e
Louis Mandel <lmandel@us.ibm.com>
2017-04-05 14:03:02 -0400

    Add conversion functions.

--- src/json_util.ml
+++ src/json_util.ml
@@ -15,3 +15,42 @@ let read_json_file reader f =
       Log.error "Json_util" None
         ("Unable to read file "^f^": "^(Printexc.to_string exn))
   end
+
+
+(** {6. Conversion functions} *)
+
+
+(** {8. workspace_response} *)
+
+let json_of_workspace_response rsp =
+  Yojson.Basic.from_string (Wcs_j.string_of_workspace_response rsp)
+
+let pretty_workspace_response rsp =
+  Yojson.Basic.pretty_to_string (json_of_workspace_response rsp)
+
+
+(** {8. pagination_response} *)
+
+let json_of_pagination_response rsp =
+  Yojson.Basic.from_string (Wcs_j.string_of_pagination_response rsp)
+
+let pretty_pagination_response rsp =
+  Yojson.Basic.pretty_to_string (json_of_pagination_response rsp)
+
+
+(** {8. list_workspaces_request} *)
+
+let json_of_list_workspaces_request req =
+  Yojson.Basic.from_string (Wcs_j.string_of_list_workspaces_request req)
+
+let pretty_list_workspaces_request req =
+  Yojson.Basic.pretty_to_string (json_of_list_workspaces_request req)
+
+
+(** {8. list_workspaces_response} *)
+
+let json_of_list_workspaces_response rsp =
+  Yojson.Basic.from_string (Wcs_j.string_of_list_workspaces_response rsp)
+
+let pretty_list_workspaces_response rsp =
+  Yojson.Basic.pretty_to_string (json_of_list_workspaces_response rsp)
