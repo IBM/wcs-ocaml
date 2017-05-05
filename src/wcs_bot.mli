c5a16f156959f4a2cb7adfa30b6ff78b22e05b06
Louis Mandel <lmandel@us.ibm.com>
2017-05-05 15:48:01 -0400

    Remove Wcs_bot.get_value and Wcs_bot.call and bypass.

--- src/wcs_bot.mli
+++ src/wcs_bot.mli
@@ -18,27 +18,17 @@
 
 open Wcs_t
 
-val bypass_default : string -> (bool * 'a) option
 val before_default : message_request -> message_request
 val after_default : message_response -> message_response
 val user_input_default : unit -> string
-val matcher_default : message_response -> 'a option
-
-val get_value :
-  ?bypass:(string -> (bool * 'a) option) ->
-  ?before:(message_request -> message_request) ->
-  ?after:(message_response -> message_response) ->
-  ?user_input:(unit -> string) ->
-  ?matcher:(message_response -> 'a option) ->
-  credential -> string -> json -> string -> string option * 'a
 
 
 val exec :
-  ?bypass:(string -> (bool * json) option) ->
   ?before:(message_request -> message_request) ->
   ?after:(message_response -> message_response) ->
   ?user_input:(unit -> string) ->
-  credential -> string -> json -> string -> json
+  credential ->
+  string -> json -> string -> json
 (**
    [exec cred ws_id ctx_init txt_init] executes the workspace [ws_id]
    with the initial context [ctx_init] and the initial input text
