59f2305992ed74ff0c3f3c3b3990c84980071a93
Jerome Simeon <simeon@us.ibm.com>
2017-10-13 03:38:02 -0400

    Add interface to Spel_top

---
+++ wcs-lib/spel_top.mli
@@ -0,0 +1,28 @@
+(*
+ *  This file is part of the Watson Conversation Service OCaml API project.
+ *
+ * Copyright 2016-2017 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *)
+
+val parse_spel_cond_from_file : string -> Spel_t.expression
+val parse_spel_cond_from_string : string -> Spel_t.expression
+
+val parse_spel_text_from_file : string -> Spel_t.expression
+val parse_spel_text_from_string : string -> Spel_t.expression
+
+val build_spel_context_from_context :
+  (string * Yojson.Basic.json) list
+  -> (string * Spel_t.json_expression) list
+
