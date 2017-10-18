df0f061c33e273972751b7420a42f5449be56920
Jerome Simeon <simeon@us.ibm.com>
2017-10-18 11:06:23 -0400

    Cleanups of Spel_builder and Spel_print interfaces

--- wcs-lib/spel_print.ml
+++ wcs-lib/spel_print.ml
@@ -201,20 +201,20 @@ let print_expression_common istop e =
   lift_constants (print_expr_aux 0 istop e)
 
 (** {6 Top level printer for Spel expressions} *)
-let print_expression e : string = print_expr_aux 0 false e
+let to_string e : string = print_expr_aux 0 false e
 
 (** {6 Top level printer for JSON with embedded Spel expressions} *)
-let rec print_json_expression j : Json_t.json =
+let rec to_json j : Json_t.json =
   begin match j with
-  | `Assoc l -> `Assoc (List.map (fun x -> (fst x, print_json_expression (snd x))) l)
+  | `Assoc l -> `Assoc (List.map (fun x -> (fst x, to_json (snd x))) l)
   | `Bool b -> `Bool b
   | `Float f -> `Float f
   | `Int i -> `Int i
-  | `List l -> `List (List.map print_json_expression l)
+  | `List l -> `List (List.map to_json l)
   | `Null -> `Null
   | `Expr e -> print_expression_common true e
   end
 
 (** {6 Auxiliary printer for text expressions} *)
-let print_text e = print_expr_aux 0 true e
+let to_text e = print_expr_aux 0 true e
 
