8b5892ee8b9ccc3fc9438e63715537024359bc79
Louis Mandel <lmandel@us.ibm.com>
2017-10-20 14:36:02 -0400

    Add main to knock knock bots.

--- examples/knock_spel.ml
+++ examples/knock_spel.ml
@@ -54,6 +54,65 @@ let knockknock =
     ~dialog_nodes: (knock who_intent entity_name "Nevermind it's pointless")
     ()
 
-let () =
-  print_endline
-    (Wcs_json.pretty_workspace knockknock)
+let main () =
+  let wcs_cred_file = ref None in
+  let ws_id = ref None in
+  let print = ref false in
+  let deploy = ref false in
+  let exec = ref false in
+  let speclist =
+    [ "-cred", Arg.String (fun s -> wcs_cred_file := Some s),
+      "cred.json The file containing the Watson Conversation Service credentials.";
+      "-id", Arg.String (fun id -> ws_id := Some id),
+      "id The workspace id used to update in conjunction with -deploy.";
+      "-print", Arg.Set print,
+      " Print the workspace on stdout.";
+      "-deploy", Arg.Set deploy,
+      " Create or update the workspace on Watson Conversation Service.";
+      "-exec", Arg.Set exec,
+      " Execute the chatbot."
+    ]
+  in
+  let usage =
+    "Usage: "^Sys.argv.(0)^" [options]"
+  in
+  Arg.parse speclist (fun _ -> ()) usage;
+  let wcs_cred = Wcs_bot.get_credential !wcs_cred_file in
+  begin match !print with
+  | true ->
+      print_endline (Wcs_json.pretty_workspace knockknock)
+  | false ->
+      ()
+  end;
+  begin match !deploy, !ws_id with
+  | true, Some ws_id ->
+      let () = Wcs.update_workspace wcs_cred ws_id knockknock in
+      Format.printf "%s: updated@." ws_id
+  | true, None ->
+      begin match Wcs.create_workspace wcs_cred knockknock with
+      | { crea_rsp_workspace_id = Some id } ->
+          Format.printf "%s: created@." id;
+          ws_id := Some id;
+      | _ -> assert false
+      end
+  | false, _ -> ()
+  end;
+  begin match !exec, !ws_id with
+  | true, Some id ->
+      let _ = Wcs_bot.exec wcs_cred id `Null "" in
+      ()
+  | false, _ ->
+      ()
+  | true, None ->
+      Arg.usage speclist "no worksapce to execute";
+      exit 1
+  end
+
+let _ =
+  begin try
+    main ()
+  with
+  | Log.Error (module_name, msg) when not !Log.debug_message ->
+      Format.eprintf "%s@." msg;
+      exit 1
+  end
