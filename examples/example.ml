af579ab66dc721a54f31550bd65753bee6cc0176
Louis Mandel <lmandel@us.ibm.com>
2017-11-10 09:46:29 -0500

    Small example cleanup.

--- examples/example.ml
+++ examples/example.ml
@@ -17,10 +17,9 @@
  *)
 
 open Wcs_t
-module Mk = Wcs
 
 let intent_help =
-  Mk.intent "help"
+  Wcs.intent "help"
     ~description:"The user needs help knowing what to do"
     ~examples:[ "help";
                 "I am confused";
@@ -35,14 +34,14 @@ let intent_help =
     ()
 
 let is_number =
-  Mk.dialog_node "Is number"
-    ~conditions: "@sys-number"
+  Wcs.dialog_node "Is number"
+    ~conditions_spel: (Spel.entity Wcs.sys_number ())
     ~text: "You have selected the number @sys-number."
     ~context: (Context.skip_user_input true)
     ()
 
 let win =
-  Mk.dialog_node "Win"
+  Wcs.dialog_node "Win"
     ~parent: is_number
     ~conditions: "@sys-number == 42"
     ~text: "You win!"
@@ -50,7 +49,7 @@ let win =
     ()
 
 let lost =
-  Mk.dialog_node "Lost"
+  Wcs.dialog_node "Lost"
     ~previous_sibling: win
     ~conditions: "anything_else"
     ~text: "Sorry, do you want to try again?"
@@ -59,20 +58,20 @@ let lost =
 
 let mk_prompt prompt_txt help_txt =
   let prompt =
-    Mk.dialog_node "Prompt"
+    Wcs.dialog_node "Prompt"
       ~conditions: "conversation_start"
       ~text: prompt_txt
       ()
   in
   let help =
-    Mk.dialog_node "Help"
+    Wcs.dialog_node "Help"
       ~conditions: "#help"
       ~text: help_txt
       ~next_step: (prompt, Goto_body)
       ()
   in
   let default =
-    Mk.dialog_node "Default"
+    Wcs.dialog_node "Default"
       ~text: "I don't understand what you say."
       ~next_step: (help, Goto_body)
       ()
@@ -80,9 +79,9 @@ let mk_prompt prompt_txt help_txt =
   [ prompt; help; default; ]
 
 let example1 =
-  Mk.workspace "Example 1"
+  Wcs.workspace "Example 1"
     ~intents: [ intent_help ]
-    ~entities: [ Mk.sys_number ]
+    ~entities: [ Wcs.sys_number ]
     ~dialog_nodes: (is_number
                     :: win
                     :: lost
