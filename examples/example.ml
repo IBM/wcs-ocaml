a8b407e83b6e8c5b41a3ecfa1687ff0e6cc2875d
Louis Mandel <lmandel@us.ibm.com>
2017-11-28 16:43:36 -0500

    Update examples.

--- examples/example.ml
+++ examples/example.ml
@@ -118,7 +118,7 @@ let main () =
     "Usage: "^Sys.argv.(0)^" [options]"
   in
   Arg.parse speclist (fun _ -> ()) usage;
-  let wcs_cred = Wcs_bot.get_credential !wcs_cred_file in
+  let wcs_cred = Wcs_bot_unix.get_credential !wcs_cred_file in
   begin match !print with
   | true ->
       print_endline (Wcs_pretty.workspace example1)
@@ -127,10 +127,10 @@ let main () =
   end;
   begin match !deploy, !ws_id with
   | true, Some ws_id ->
-      let () = Wcs_api.update_workspace wcs_cred ws_id example1 in
+      let () = Wcs_api_unix.update_workspace wcs_cred ws_id example1 in
       Format.printf "%s: updated@." ws_id
   | true, None ->
-      begin match Wcs_api.create_workspace wcs_cred example1 with
+      begin match Wcs_api_unix.create_workspace wcs_cred example1 with
       | { crea_rsp_workspace_id = Some id } ->
           Format.printf "%s: created@." id;
           ws_id := Some id;
@@ -140,7 +140,7 @@ let main () =
   end;
   begin match !exec, !ws_id with
   | true, Some id ->
-      let _ = Wcs_bot.exec wcs_cred id (`Assoc [ "fact", `String id]) "" in
+      let _ = Wcs_bot_unix.exec wcs_cred id (`Assoc [ "fact", `String id]) "" in
       ()
   | false, _ ->
       ()
